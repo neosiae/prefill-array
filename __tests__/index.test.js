04618098eb3c3af3c6238c8116646b6b91f53b0f
Denis Kotlica <neosiae@gmail.com>
2018-04-07 23:59:19 +0200

    Initial commit

---
+++ __tests__/index.test.js
@@ -0,0 +1,26 @@
+const prefill = require('../index');
+const test = require('tape');
+
+test('it works with numbers greater than 0', t => {
+  const expected = [1, 1, 1, 1, 1];
+  const actual = prefill(5, 1);
+
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test('it works with number as 0', t => {
+  const expected = [];
+  const actual = prefill(0);
+
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test('it works with undefined value', t => {
+  const expected = [undefined, undefined, undefined];
+  const actual = prefill(3);
+
+  t.deepEqual(actual, expected);
+  t.end();
+});
---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = (number, value) => {
+  const typeOfNumber = typeof number;
+  const isInteger = Number.isInteger(number);
+
+  if (typeOfNumber !== 'number' && !isInteger) {
+    throw new TypeError(
+      `Number type is invalid. Expected an integer, but got: ${typeOfNumber}`,
+    );
+  }
+
+  return Array.from(new Array(number), () => value);
+};
